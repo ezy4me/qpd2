4dbbf7e32bc24931c01ad45953c430a789a26735
ezy4me <mrronal@mail.ru>
2024-09-12 14:06:53 +0300

    add: favicon, title and desc

--- src/components/common/BackgroundLines/BackgroundLines.tsx
+++ src/components/common/BackgroundLines/BackgroundLines.tsx
@@ -1,7 +1,27 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import styles from "./BackgroundLines.module.scss";
 
 const BackgroundLines = () => {
-  const lines = new Array(23).fill(null); 
+  const [lineCount, setLineCount] = useState(23);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) { 
+        setLineCount(12);
+      } else {
+        setLineCount(23);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const lines = new Array(lineCount).fill(null);
 
   return (
     <div className={styles["absolute-lines-wrapper"]}>
